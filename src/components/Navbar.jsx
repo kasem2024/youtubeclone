f9549d9b04c471bdb0c399e2e78ac3604114bc64
KaSeM5 <kasem200mohareb@gmail.com>
2024-06-03 14:18:39 +0300

    change website icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { SearchBar } from "./";
 const Navbar = () => (
   <Stack direction="row" alignItems="center" p={2} sx={{ position:  "sticky", background: '#000', top: 0, justifyContent: "space-between" }}>
     <Link to="/" style={{ display: "flex", alignItems: "center" }} >
-      <img src={logo} alt="logo" height={100} width={200}/>
+      <img src={logo} alt="logo" height={70} width={70}/>
     </Link>
     <SearchBar />
   </Stack>
